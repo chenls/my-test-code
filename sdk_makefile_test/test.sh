40ea8e87269e1cb21330f0ab8c2f9f49740454b2
liusheng.chen <liusheng.chen@jiiov.com>
2021-09-01 14:01:51 +0800

    链接优化对比

--- sdk_makefile_test/test.sh
+++ sdk_makefile_test/test.sh
@@ -9,9 +9,19 @@ make OPTLEVEL=-O3 ARCH=qsee-$ARCH_SUFFIX -C ./ NEED_MANGLE=1 VENDOR_NAME=${vendo
 # make -n
 # make -d -p
 make OPTLEVEL=-O3 ARCH=qsee-$ARCH_SUFFIX -C ./ NEED_MANGLE=1 VENDOR_NAME=${vendor_name} PROJECT_NAME=${PROJECT_NAME} LIMIT_TIME=${LimitTime}
-
-clang --target=aarch64-none-linux-android28 main.c anctee__aarch64.o -o test
+clang --target=aarch64-none-linux-android28 main.c anctee__aarch64.o -o test -Wl,-gc-sections
+ls -al anctee__aarch64.o test
 adb push test /data/local/tmp
 echo
 adb shell /data/local/tmp/test
+
+clang --target=aarch64-none-linux-android28 -ffunction-sections -fdata-sections -c mylib1.c -o mylib1.o
+clang --target=aarch64-none-linux-android28 -ffunction-sections -fdata-sections -c mylib2.c -o mylib2.o
+aarch64-linux-gnu-ld -r mylib1.o mylib2.o -o mylib.o
+clang --target=aarch64-none-linux-android28 main.c mylib.o -o test2 -Wl,-gc-sections
+ls -al mylib.o test2
+adb push test2 /data/local/tmp
+echo
+adb shell /data/local/tmp/test2
+
 cd - &> /dev/null
