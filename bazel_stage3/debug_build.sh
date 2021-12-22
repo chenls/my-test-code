8037f028136d93807ef1917211ee336850dc35c2
liusheng.chen <liusheng.chen@jiiov.com>
2021-12-22 11:22:31 +0800

    gdb debug

--- bazel_stage3/debug_build.sh
+++ bazel_stage3/debug_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 bazel clean
-bazel build -s //main:hello-world &> build.log
+bazel build -s //main:hello-world -c dbg &> build.log
 ln -sf bazel-out/../../../external .
 python generate_compile_commands.py build.log compile_commands.json
+
+# bazel query "deps(//main:hello-world)"
