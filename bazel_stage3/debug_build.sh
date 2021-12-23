4cccbdbebcf1ff870eb5afde16f89c5c2f827091
liusheng.chen <liusheng.chen@jiiov.com>
2021-12-23 18:05:47 +0800

    new compile_commands.json

--- bazel_stage3/debug_build.sh
+++ bazel_stage3/debug_build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-bazel clean
-bazel build -s //main:hello-world -c dbg &> build.log
-ln -sf bazel-out/../../../external .
-python generate_compile_commands.py build.log compile_commands.json
 
 # bazel query "deps(//main:hello-world)"
+
+# bazel clean
+# bazel build -s //main:hello-world -c dbg &> build.log
+# ln -sf bazel-out/../../../external .
+# python generate_compile_commands.py build.log compile_commands.json
+
+
+if [ ! -f "tools/actions/BUILD" ]; then
+    sh ./Bazel_and_CompileCommands/setup_compile_commands.sh
+fi
+sh ./Bazel_and_CompileCommands/create_compile_commands.sh //main:hello-world
+ln -sf bazel-out/../../../external .
