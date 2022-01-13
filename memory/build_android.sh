ee5b490035561d7c15c55a5baac8a272e40d0c08
liusheng.chen <liusheng.chen@jiiov.com>
2022-01-13 17:48:47 +0800

    Add android build

--- memory/build_android.sh
+++ memory/build_android.sh
@@ -5,11 +5,15 @@ cd $dir
 mkdir -p build
 rm -rf build/*
 cd build
-cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+-DANDROID_PLATFORM=android-28 \
+..
+
 bear make VERBOSE=1
 cd -
 ln -sf build/compile_commands.json compile_commands.json
 file ./build/test
 echo ""
-./build/test
+adb push ./build/test /data/local/tmp/test_666
+adb shell /data/local/tmp/test_666
 echo ret:$?
